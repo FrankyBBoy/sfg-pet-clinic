bc3381af1a0ac8954a577c6af7be176b84cf4aaf
FrankyBBoy <francis1592@gmai.com>
2021-09-22 11:02:29 -0400

    Lecture 155 - Create JPA Services

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialityRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialityRepository.java
@@ -1,7 +1,7 @@
 package guru.springframework.sfgpetclinic.repositories;
 
 import guru.springframework.sfgpetclinic.model.Speciality;
-import guru.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.data.repository.CrudRepository;
 
-public interface SpecialityRepository extends CrudService<Speciality, Long> {
+public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
 }
